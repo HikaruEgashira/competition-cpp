dbe8599fc088a944bc5bbd82510559c816555d11
HikaruEgashira <shighten@gmail.com>
2019-06-19 14:16:18 +0900

    ver 3.0

--- 02cptest.sh
+++ 02cptest.sh
@@ -3,16 +3,15 @@ problemname=$1
 script_dir=$(cd $(dirname $0); pwd)
 src_dir=${script_dir}/src
 
-# 本番中に使うためにログインする
 oj login "https://${problemname:0:6}.contest.atcoder.jp/tasks/${problemname:0:8}" 
 
-# testディレクトリがなければ取得する
+# testディレクトリにすでにテストがある場合はスキップ
 if [ ! -e test/$1 ]; then
-  rm -rf test/
+  rm -rf test/*
   oj dl "https://${problemname:0:6}.contest.atcoder.jp/tasks/${problemname:0:8}"
 fi
 touch test/$1
 
-g++ -Wall -std=c++14 ${src_dir}/${problemname:0:6}/$1.cpp
+g++ -Wall -std=c++14 ${src_dir}/${problemname:0:6}/$1.cc
 oj test
 rm -f a.out
